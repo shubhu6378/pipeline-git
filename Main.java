6668fcffd222d4f1e9643b8e71154903e1f434a3
Shubham Kumar <shubhamrock65789@gmail.com>
2023-12-23 16:49:01 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String args[]){ 
         int a=10;
         int b=20;
-        int c; 
+        String c; 
     	System.out.println("Hello Java");  
     	System.out.println("Hello Shubham");
         System.out.println("Hello Shubham, Jenkins Pipeline running fine");
